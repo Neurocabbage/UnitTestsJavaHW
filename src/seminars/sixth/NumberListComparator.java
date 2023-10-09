5ae5a8cc839a806708d099345779ab1d8244821b
al <paet@mail.ru>
2023-10-09 12:29:49 +0500

    add test Checkstyle.PNG

--- src/seminars/sixth/NumberListComparator.java
+++ src/seminars/sixth/NumberListComparator.java
@@ -2,8 +2,19 @@ package seminars.sixth;
 
 import java.util.List;
 
+/**
+ * Для сравнения средних значений чисел в списке.
+ */
 public class NumberListComparator {
 
+    /**
+     * Сравнение среднего значения списков чисел.
+
+     * @param list1 Первый список.
+
+     * @param list2 Второй список.
+     * @return Возвращается строка с результатом сравнения.
+     */
     public String compareLists(List<Integer> list1, List<Integer> list2) {
         double average1 = calculateAverage(list1);
         double average2 = calculateAverage(list2);
@@ -17,6 +28,13 @@ public class NumberListComparator {
         }
     }
 
+
+    /**
+     * вычисление среднего значения в списке.
+
+     * @param list Принимает список.
+     * @return Возвращается среднее значение списка.
+     */
     private double calculateAverage(List<Integer> list) {
         if (list == null || list.isEmpty()) {
             return 0;
